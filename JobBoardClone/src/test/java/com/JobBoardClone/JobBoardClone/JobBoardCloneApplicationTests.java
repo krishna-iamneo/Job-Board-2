920dec55f4203ea2f5fd45256301aa912ea33e16
unknown <shivam.pandey@iamneo.ai>
2024-04-01 12:00:23 +0530

    Features added

---
+++ JobBoardClone/src/test/java/com/JobBoardClone/JobBoardClone/JobBoardCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobBoardClone.JobBoardClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
